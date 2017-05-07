b657fda36a1e630022ffaf8ab9cfe6687fdc1d50
Nikola Jovanović <njov996@gmail.com>
2017-05-07 23:41:24 +0000

    uklonjene nekoriscene funkcije iz main

--- src/main/Main.java
+++ src/main/Main.java
@@ -31,57 +31,5 @@ public class Main {
 		}
 		MainView mainView = MainView.getInstance();
 		mainView.setVisible(true);
-		// mock();
-	}
-
-	public static void mock() {
-		Package geo = Warehouse.getInstance().getPackages().get(0);
-		Package sport = Warehouse.getInstance().getPackages().get(1);
-		doLoad("src/res/mocks/MojaBaza/Drzava.txt,Drzava", geo);
-		doLoad("src/res/mocks/MojaBaza/FudbalskiKlub.txt,FudbalskiKlub", sport);
-		doLoad("src/res/mocks/MojaBaza/Grad.txt,Grad", geo);
-	}
-
-	public static void doLoad(String csvFile, Package pkg) {
-		String[] bla = csvFile.split(",");
-		Entity entity = null;
-		for (Entity e : pkg.getEntities()) {
-			if (e.getName().equals(bla[1])) {
-				entity = e;
-				break;
-			}
-		}
-
-		String line = "";
-		String cvsSplitBy = ",";
-		try (BufferedReader br = new BufferedReader(new FileReader(bla[0]))) {
-			line = br.readLine();
-			String[] names = line.split(cvsSplitBy);
-			while ((line = br.readLine()) != null) {
-				String[] fields = line.split(cvsSplitBy);
-				Record en = new Record(entity);
-				for (int i = 0; i < names.length; i++) {
-					for (Attribute a : entity.getAttributes()) {
-						if (a.getName().equals(names[i])) {
-							Object val = null;
-							Class<?> c = a.getValueClass();
-							if (c == Integer.class)
-								val = Integer.parseInt(fields[i]);
-							if (c == String.class)
-								val = fields[i];
-							if (c == Boolean.class)
-								val = fields[i].equals("true");
-
-							en.addAttribute(a, val);
-						}
-					}
-				}
-
-				// TODO
-				// entity.addEntry(en);
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 	}
 }
