2bfc0da3235cadd36c7c0bf9aa3181377a1e3a82
Ognjen Đuričić <ogicar000@gmail.com>
2017-06-10 02:52:12 +0000

    Zapocet GUI za Add, Update, Delete, Search operacije. #20969

---
+++ src/controller/BaseSearchDialogController.java
@@ -0,0 +1,13 @@
+package controller;
+
+import view.BaseSearchDialog;
+
+public class BaseSearchDialogController {
+	
+	private BaseSearchDialog view;
+	
+	public BaseSearchDialogController(BaseSearchDialog view) {
+		this.view = view;
+	}
+
+}
